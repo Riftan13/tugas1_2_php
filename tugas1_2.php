7b0528553a5ccbec1a84f6ee5046f15e3f75aef1
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-07 10:25:49 +0700

    Add files via upload

---
+++ tugas1_2.php
@@ -0,0 +1,47 @@
+<?php
+	$cireng = "Cireng";
+	$esDuren = "Es Duren";
+	$esMilo = "Es Milo";
+	$esTeh = "Es Teh";
+	$piscok = "Piscok";
+	$harga1="Rp 1000";
+	$harga2="Rp 15000";
+	$harga3="Rp 20000";
+	$harga4="Rp 5000";
+	$harga5="Rp 1000";
+
+echo "<h1>DAFTAR MENU</h1> <br>";
+
+echo "<table border =1>
+		<tr>
+			<td>No</td>
+			<td>Nama Menu</td>
+			<td>Harga</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>".$cireng."</td>
+			<td>".$harga1."</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>".$esDuren."</td>
+			<td>".$harga2."</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>".$esMilo."</td>
+			<td>".$harga3."</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>".$esTeh."</td>
+			<td>".$harga4."</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td>".$piscok."</td>
+			<td>".$harga5."</td>
+		</tr>
+		</table>";
+?>
